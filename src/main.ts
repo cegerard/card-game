10fef63c99cb084502b695726e5d843f01602d0c
Cédric GERARD <git@eriatand.org>
2024-11-05 01:14:44 +0100

    Get port from env var

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
