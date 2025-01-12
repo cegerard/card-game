91b83efa7d4c9640be9acb73658eacf670662789
Cédric GERARD <git@eriatand.org>
2025-01-12 02:18:24 +0100

    Add Special interface and implement it in SpecialAttack class for energy management

--- src/core/cards/skills/special-attack.ts
+++ src/core/cards/skills/special-attack.ts
@@ -1,18 +1,19 @@
 import { TargetingCardStrategy } from '../../targeting-card-strategies/targeting-card-strategy';
+import { Special } from './special';
 
 const ENERGY_INCREASE_FACTOR = 10;
 const CRITICAL_RATE = 1.3;
 const DEFAULT_DAMAGE_RATE = 1;
 
-export class SpecialAttack {
+export class SpecialAttack implements Special {
   constructor(
     private readonly damageRate: number,
     private readonly energyNeeded: number,
     public readonly targetingStrategy: TargetingCardStrategy,
   ) {}
 
-  public ready(energy: number): boolean {
-    return energy >= this.energyNeeded;
+  public ready(actualEnergy: number): boolean {
+    return actualEnergy >= this.energyNeeded;
   }
 
   public computeDamage(damage: number, isCritical: boolean): number {
@@ -21,7 +22,11 @@ export class SpecialAttack {
     return Math.round(damage * this.damageRate * damageMultiplier);
   }
 
-  public increaseEnergy(energy: number): number {
-    return Math.min(energy + ENERGY_INCREASE_FACTOR, this.energyNeeded);
+  public increaseEnergy(actualEnergy: number): number {
+    return Math.min(actualEnergy + ENERGY_INCREASE_FACTOR, this.energyNeeded);
+  }
+
+  public getSpecialKind(): string {
+    return 'specialAttack';
   }
 }
---
+++ src/core/cards/skills/special.ts
@@ -0,0 +1,27 @@
+/**
+ * Interface representing a special skill in the card game.
+ */
+export interface Special {
+  /**
+   * Determines if the special skill is ready to be used based on the current energy level.
+   *
+   * @param actualEnergy - The current energy level.
+   * @returns A boolean indicating whether the special skill is ready.
+   */
+  ready(actualEnergy: number): boolean;
+
+  /**
+   * Increases the current energy level.
+   *
+   * @param actualEnergy - The current energy level.
+   * @returns The new energy level after the increase.
+   */
+  increaseEnergy(actualEnergy: number): number;
+
+  /**
+   * Get the kind of special skill.
+   *
+   * @returns The kind of special skill.
+   */
+  getSpecialKind(): string;
+}
