578f6d1950b228af4707b4eca7637b280bdb04b4
Cédric GERARD <git@eriatand.org>
2025-06-13 00:06:05 +0200

    Add unit tests for MathRandomizer class to validate integer generation between specified ranges

---
+++ src/fight/tools/__tests__/math-randomizer.spec.ts
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker/.';
+
+import { MathRandomizer } from '../math-randomizer';
+
+describe('math randomizer', () => {
+  const randomizer = new MathRandomizer();
+  describe('when computing a integer between two values', () => {
+    describe('when max is strictly higher than min', () => {
+      const min = faker.number.int(10);
+      const max = faker.number.int({ min, max: 100 });
+      it('returns the int value', () => {
+        const randomValue = randomizer.random_int_between(min, max);
+
+        expect(randomValue).toBeGreaterThanOrEqual(min);
+        expect(randomValue).toBeLessThanOrEqual(max);
+      });
+    });
+
+    describe('when max is equal to min', () => {
+      const min = faker.number.int(100);
+      const max = min;
+      it('returns the min/max value', () => {
+        const randomValue = randomizer.random_int_between(min, max);
+
+        expect(randomValue).toEqual(min);
+      });
+    });
+  });
+});
