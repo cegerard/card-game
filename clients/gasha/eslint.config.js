0825704cfebb839a84a5e14161912050dad1e7bf
Cédric GERARD <git@eriatand.org>
2026-06-17 21:30:14 +0200

    fix lint

--- clients/gasha/eslint.config.js
+++ clients/gasha/eslint.config.js
@@ -3,9 +3,17 @@ import ts from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import svelte from 'eslint-plugin-svelte';
 import svelteParser from 'svelte-eslint-parser';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
   {
     files: ['**/*.ts'],
     languageOptions: {
